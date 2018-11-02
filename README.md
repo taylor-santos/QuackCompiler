914093eaaf81a8a593b840d7c2f442ba3cff65c3
taylor-santos <taylor.p.santos@gmail.com>
2018-11-02 16:16:42 -0700

    AST Generation

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# Quack AST - Taylor Santos
+# reflex-bison-ast
 
-Cloned from https://github.com/UO-cis561/reflex-bison-ast. Modified to parse Quack (.qk) source code and build an Abstract Syntax Tree. Outputs the resulting AST as json to stdout.
-Successfully parsed files will print the message "Finished parse with no errors" to stderr. Otherwise, information about the location of an error, the encountered symbol, and the expected token will be printed followed by "Unable to parse!". 
+Cloned from https://github.com/UO-cis561/reflex-bison-ast. Modified to parse Quack (.qk) source code. 
+Successfully parsed files will print the message "Finished parse with no errors". Otherwise, information about the location of an error, the encountered symbol, and the expected token will be printed followed by "Unable to parse!". 
 
 ## Building
 Run the following commands from the project's root directory:
--- parser.cpp
+++ parser.cpp
@@ -6,18 +6,23 @@
 #include "lex.yy.h"
 #include "EvalContext.h"
 #include "Messages.h"
+#include "AST.h"
 
 class Driver {
 public:
-    explicit Driver(reflex::Input in) : lexer(in), parser(new yy::parser(lexer)){}
+    explicit Driver(reflex::Input in) : lexer(in), parser(new yy::parser(lexer, &root)){ root = NULL; }
     ~Driver() { delete parser; }
     bool parse() {
         int result = parser->parse();
         return result == 0 && report::ok();
     }
+    void json(std::ostream &out){
+        root->json(out);
+    }
 private:
-    yy::Lexer   lexer;
-    yy::parser *parser;
+    yy::Lexer     lexer;
+    yy::parser   *parser;
+    AST::ASTNode *root;
 };
 
 int main()
@@ -25,8 +30,9 @@ int main()
     Driver driver(std::cin);
     bool parse_result = driver.parse();
     if (parse_result) {
-        std::cout << "Finished parse with no errors" << std::endl;
+        std::cerr << "Finished parse with no errors" << std::endl;
+        driver.json(std::cout);
     } else {
-        std::cout << "Unable to parse!" << std::endl;
+        std::cerr << "Unable to parse!" << std::endl;
     }
 }
